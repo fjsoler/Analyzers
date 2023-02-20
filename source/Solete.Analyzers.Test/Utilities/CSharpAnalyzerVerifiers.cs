8cf5b10f27ae1e4ab6ef1787279790b8aab0be95
Fco Javier Soler <javiersolerc@gmail.com>
2023-02-20 01:23:45 +0100

    Missing dependency

--- source/Solete.Analyzers.Test/Utilities/CSharpAnalyzerVerifiers.cs
+++ source/Solete.Analyzers.Test/Utilities/CSharpAnalyzerVerifiers.cs
@@ -1,8 +1,5 @@
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Diagnostics;
