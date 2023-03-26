db44a2597824fd521354652beb919f26071380a3
Fco Javier Soler <javiersolerc@gmail.com>
2023-03-27 00:55:43 +0200

    delete path

--- template/InstallTemplate.ps1
+++ template/InstallTemplate.ps1
@@ -1,5 +1,5 @@
 # Install template
 # Delete folders that are not included in template (bin,obj,.idea)
-Get-ChildItem -Recurse -Directory -Include bin,obj,.idea | Remove-Item -Recurse -Force *> D:\Javier\Projects\Analyzers\template\install.log
+Get-ChildItem -Recurse -Directory -Include bin,obj,.idea | Remove-Item -Recurse -Force *
 # Install the template
-dotnet new install .\working\templates\analyzer --force >> D:\Javier\Projects\Analyzers\template\install.log
+dotnet new install .\working\templates\analyzer --force
